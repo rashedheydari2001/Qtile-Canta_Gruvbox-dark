2f8a0358bb3d985bab1801f742243aae7c1b2a89
rashedheydari2001 <81459372+rashedheydari2001@users.noreply.github.com>
2022-07-09 20:59:17 +0430

    Update autostart.sh

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -1,4 +1,4 @@
 #!/bin/env bash
-setxkbmap -model pc105 -layout us,ir -option grp:alt_shift_toggle -option numpad:microsoft
+setxkbmap -model pc105 -layout us,uk -option grp:alt_shift_toggle -option numpad:microsoft
 feh --bg-scale "/home/rashed/Pictures/wallpaper/2.jpg"
 picom --experimental-backend --config .config/picom/picom.conf &
